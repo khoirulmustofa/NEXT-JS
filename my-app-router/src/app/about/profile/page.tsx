fe32033c7ff561481b76c15ebcddcdac732f0306
Khoirul Mustofa <khoirul.mustofa2014@gmail.com>
2024-02-16 21:01:36 +0700

    Setup Project

---
+++ my-app-router/src/app/about/profile/page.tsx
@@ -0,0 +1,10 @@
+import Photo from "./photo";
+
+export default function ProfilePage() {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      <Photo></Photo>
+    </div>
+  );
+}
---
+++ my-app-router/src/app/about/profile/photo.tsx
@@ -0,0 +1,7 @@
+export default function Photo() {
+    return (
+        <div>
+            <h5>Ini Photo</h5>
+        </div>
+    )
+}
